f870c829dd7402ce1f570335933744901853f95f
Tommy Volk <tvolk131@gmail.com>
2025-02-26 11:16:14 -0600

    feat: export currency lists

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,5 +1,8 @@
-export type FiatCurrency = "USD" | "EUR" | "GBP";
-export type AnyCurrency = FiatCurrency | "BTC" | "USDT";
+export const FIAT_CURRENCIES = ["USD", "EUR", "GBP"] as const;
+export const ALL_CURRENCIES = [...FIAT_CURRENCIES, "BTC", "USDT"] as const;
+
+export type FiatCurrency = (typeof FIAT_CURRENCIES)[number];
+export type AnyCurrency = (typeof ALL_CURRENCIES)[number];
 
 export type Amount<T extends string> = {
   amount: string;
